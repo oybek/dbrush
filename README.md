813a6e2222a84cde0ac26575eee6ac5a0c6772df
oybek <hashimov@tochka.com>
2020-07-02 21:55:18 +0500

    added tests for postgres 10.*, update README.md

--- README.md
+++ README.md
@@ -1,3 +1,68 @@
-# DBRUSH
+# DB RUSH
 
-Super simple migration tool for scala (supported dbs: postgres)
+[![Lines of Code](https://sonarcloud.io/api/project_badges/measure?project=oybek_db-rush&metric=ncloc)](https://sonarcloud.io/dashboard?id=oybek_db-rush)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=oybek_db-rush&metric=coverage)](https://sonarcloud.io/dashboard?id=oybek_db-rush)
+
+*Super simple pure migration tool for scala*
+
+Traditional migration tools such as liquibase or flyway works with externally
+described migration file.
+
+**DB RUSH** is a simple library to do migrations right in scala code.
+
+```scala
+lazy val createCity =
+  sql"""
+       |create table city(
+       |  name varchar,
+       |  population int not null
+       |)
+       |""".stripMargin
+
+lazy val createSchool =
+  sql"""
+       |create table school(
+       |  name varchar,
+       |  latitude float,
+       |  longitude float
+       |)
+       |""".stripMargin
+
+lazy val createStudent =
+  sql"""
+       |create table Student(
+       |  name varchar,
+       |  age int
+       |)
+       |""".stripMargin
+
+lazy val plugFuzzySearch =
+  sql"CREATE EXTENSION fuzzystrmatch"
+
+// Migration list
+List(
+  Migration("create city", createCity, createSchool),
+  Migration("create student", createStudent),
+  Migration("plug fuzzy search", plugFuzzySearch)
+).exec(transactor)
+```
+
+Each migration executed within one transaction.
+Only appends to migration list allowed, otherwise you'll get consistency
+hash (md5) mismatch at migration phase.
+
+Library creates `db_rush_commits` for usage:
+```sql
+gdetram=# select * from db_rush_commits;
+ index |       label       |               md5
+-------+-------------------+----------------------------------
+     1 | create city       | 6AD188238A2F3B19E0AE1FEBB7383992
+     2 | create student    | 26E31E875DB55C8746728D6B789289BE
+     3 | plug fuzzy search | 90B3A78FA8FE18CE36EB56F3C3A54494
+(3 rows)
+```
+
+Tested under Postgresql:
+
+* 10.[2-13]
+* 9.6.12
--- src/test/scala/io/github/oybek/dbrush/db/QueriesSpec.scala
+++ src/test/scala/io/github/oybek/dbrush/db/QueriesSpec.scala
@@ -1,7 +1,7 @@
 package io.github.oybek.dbrush.db
 
 import cats.effect.IO
-import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
+import com.dimafeng.testcontainers.{Container, ForAllTestContainer, PostgreSQLContainer}
 import doobie.scalatest.IOChecker
 import doobie.util.ExecutionContexts
 import doobie.util.transactor.Transactor
@@ -9,9 +9,8 @@ import io.github.oybek.dbrush.Engine._
 import io.github.oybek.dbrush.model.Migration
 import org.scalatest.funsuite.AnyFunSuite
 
-class QueriesSpec extends AnyFunSuite with IOChecker with ForAllTestContainer {
-
-  override val container = PostgreSQLContainer()
+trait QueriesSpec extends AnyFunSuite with IOChecker with ForAllTestContainer {
+  def container: PostgreSQLContainer
   implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
 
   lazy val transactor =
@@ -32,3 +31,18 @@ class QueriesSpec extends AnyFunSuite with IOChecker with ForAllTestContainer {
     check(Queries.insertCommit("foo", "bar"))
   }
 }
+
+class `PG_10_13` extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.13") }
+class `PG_10_12` extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.12") }
+class `PG_10_11` extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.11") }
+class `PG_10_10` extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.10") }
+class `PG_10_9`  extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.9") }
+class `PG_10_8`  extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.8") }
+class `PG_10_7`  extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.7") }
+class `PG_10_6`  extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.6") }
+class `PG_10_5`  extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.5") }
+class `PG_10_4`  extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.4") }
+class `PG_10_3`  extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.3") }
+class `PG_10_2`  extends QueriesSpec { override val container = PostgreSQLContainer("postgres:10.2") }
+
+class `PG_9_6_12` extends QueriesSpec { override val container = PostgreSQLContainer("postgres:9.6.12") }
