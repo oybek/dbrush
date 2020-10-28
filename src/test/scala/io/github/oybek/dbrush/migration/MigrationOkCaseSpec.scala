b6c9bcf2851b1ca68b5c05298df3094ea66b0401
oybek <hashimov@tochka.com>
2020-10-28 20:24:12 +0500

    fix

--- src/test/scala/io/github/oybek/dbrush/migration/MigrationOkCaseSpec.scala
+++ src/test/scala/io/github/oybek/dbrush/migration/MigrationOkCaseSpec.scala
@@ -1,19 +1,12 @@
 package io.github.oybek.dbrush.migration
 
-<<<<<<< HEAD
 import cats.effect.IO
 import cats.implicits.catsSyntaxEitherId
 import doobie.implicits._
-import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
-=======
-import cats.implicits.catsSyntaxEitherId
-import doobie.implicits._
-import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
-import .F
->>>>>>> 26c95482a81781d1a06d30b7b43e545a8076e768
 import io.github.oybek.dbrush.model.Migration
 import io.github.oybek.dbrush.syntax.MigrationsOps
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
 import scala.collection.mutable.ArrayBuffer
 
 class MigrationOkCaseSpec extends Common {
