26c95482a81781d1a06d30b7b43e545a8076e768
oybek <hashimov@tochka.com>
2020-10-28 18:42:14 +0500

    write good migration case spec

--- src/main/scala/io/github/oybek/dbrush/syntax/package.scala
+++ src/main/scala/io/github/oybek/dbrush/syntax/package.scala
@@ -7,9 +7,9 @@ package object syntax {
 
   implicit class MigrationsOps(val migrations: List[Migration]) extends AnyVal {
     def exec[F[_]: BracketThrow](
-                                  tx: Transactor[F],
-                                  log: Option[String => F[Unit]] = None
-                                ): F[Unit] = MigrationExecutor.make[F](tx, log).apply(migrations)
+        tx: Transactor[F],
+        log: Option[String => F[Unit]] = None
+    ): F[Unit] = MigrationExecutor.make[F](tx, log).apply(migrations)
   }
 
 }
---
+++ src/test/scala/io/github/oybek/dbrush/migration/MigrationOkCaseSpec.scala
@@ -0,0 +1,43 @@
+package io.github.oybek.dbrush.migration
+
+import cats.implicits.catsSyntaxEitherId
+import doobie.implicits._
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import .F
+import io.github.oybek.dbrush.model.Migration
+import io.github.oybek.dbrush.syntax.MigrationsOps
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+class MigrationOkCaseSpec extends Common {
+
+  override def afterStart(): Unit = ()
+
+  private val createCity =
+    sql"""
+         |create table city(
+         |  name varchar,
+         |  population int not null
+         |)
+         |""".stripMargin
+
+  private val createStudent =
+    sql"""
+         |create table student(
+         |  name varchar,
+         |  age int
+         |)
+         |""".stripMargin
+
+  test("when all migration success") {
+    Slf4jLogger
+      .create[F]
+      .flatMap { logger =>
+        List(
+          Migration("create city", createCity),
+          Migration("create student", createStudent)
+        ).exec(transactor, Some(logger.info(_: String)))
+      }
+      .attempt
+      .unsafeRunSync() shouldBe ().asRight[Throwable]
+  }
+}
